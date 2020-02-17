8fe2817c458cfc462bc487aae622e979aa79e3e6
Tyler Friedberg <thefriedberger@gmail.com>
2020-02-17 10:59:55 -0500

    Apparently hadn't saved changes
    Should work now

--- Assets/Scripts/MissionDemolition.cs
+++ Assets/Scripts/MissionDemolition.cs
@@ -62,7 +62,7 @@ public class MissionDemolition : MonoBehaviour
     }
 
     void UpdateGUI() {
-        uitLevel.text = "Level: " + (level + 1) + " of" + levelMax;
+        uitLevel.text = "Level: " + (level + 1) + " of " + levelMax;
         uitShots.text = "Shots Taken: " + shotsTaken;
     }
 
